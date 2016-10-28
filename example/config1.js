118f90d2c22903d0720a5cd6f1177831ee6f1915
haydeeartaza <haydee.artaza@tgac.ac.uk>
2016-10-28 16:48:38 +0100

    rename test

---
+++ example/config1.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '8LxBnmnYrjo8smSW83fvCcUPq'
+  , consumer_secret: 'pxC3kuK7dN1EVZD9lW5VKTc71j53DLcnZvay9DE7xT0y2GO8uH'
+  , access_token: '3301871662-HXBC4LQWhGBYZGzSXUYuwVZJD4yx2dZpETIJ8Q7'
+  , access_token_secret: 'hQr1DLKpghBFShk2nbY6ekHD68PVM8WZpzArryY4Nhmj4'
+}
---
+++ example/example-twit.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var Hits = require('../hits');
+var config = require('./config1');
+var hits = new Hits(config);
+var file_output = 'result_twitter.txt';
+var encoding = 'utf8';
+
+var keyword = process.argv[2]; //'swissprot,swiss prot,swiss-prot,UniProtKB,uniprot' 
+var timescan = 60; //seconds
+
+
+console.log('Running twitter metric');
+
+hits.getHits(keyword, timescan, function (results){
+	console.log(results);
+});
+
